e2fef5e04a43ab5005e4c2e7dcfcd9fabf580441
Ivan <forwebandstudying@gmail.com>
2019-11-26 19:51:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+'use strict'
+
+
+//first lesson
+/*
+var leftBorderWidth = 1;
+let second = 2;
+const pi = 3.14;
+console.log(second); */
+
+ //second lesson 
+/*
+let string = 'string';
+let number = 123;
+let boolean = true; // or false
+let und; //variable value is  undefined
+let nl = null;
+let obj = {};
+let sym = Symbol('s');
+
+let infinity = 4 / 0; // variable value is infinity
+console.log(infinity);
+console.log(sym);
+
+let persone = {
+    name: "John",
+    age: 23,
+    isMarried: false
+}
+
+console.log(persone.name);
+console.log(persone["age"]);
+
+let arr = ['apple', true, 123];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]); */
+
+//third lesson
+/* alert('I\'\m ok ');
+let question = confirm('Are you use google chrome ?');
+console.log(question); 
+
+let question = prompt('How old are you?', 23);
+console.log(question);
+console.log(typeof (question));*/
+
+// fourth lesson
+/* console.log(4 + '2'); //42 - string
+console.log(4 + +'2'); //6 - number */
+
+/* let incr = 10,
+    decr = 10;
+
+console.log(++incr); //11
+console.log(incr++); //10
+
+console.log(decr--); //10
+console.log(--decr); //9 */
+
+console.log(5 % 2); //1 остаток от деления
+console.log('2'== 2); //true
+console.log('2'=== 2); //false
+
+let a = true,
+    b = true,
+    c = false,
+    d = false;
+console.log(a && b); // true
+console.log(a && c); // fasle
+
+console.log(a || b); // true
+console.log(a || c); // true
+console.log(c || d); // fasle
+console.log(c || !d);// true
+
+
+
+
+
+
